ecd704fdc8350d6a246618c21643002cc44a965e
JosueSB <a20206724@pucp.edu.pe>
2025-11-12 13:43:57 -0500

    Update VueloRepository.java

--- server/src/main/java/com/pucp/dp1/grupo4d/morapack/repository/VueloRepository.java
+++ server/src/main/java/com/pucp/dp1/grupo4d/morapack/repository/VueloRepository.java
@@ -26,9 +26,11 @@ public interface VueloRepository extends JpaRepository<VueloEntity, Integer> {
             ad.codigo AS destino_codigo,
             v.fecha_hora_salida_utc AS fecha_salida,
             v.fecha_hora_llegada_utc AS fecha_llegada,
-            (p.capacidad - v.capacidad_disponible) AS capacidad_ocupada
+            l.tamanio AS capacidad_ocupada
         FROM vuelo v
         JOIN plan p ON v.id_plan = p.id
+        join ruta_por_vuelo rv on rv.id_vuelo=v.id
+        join lote l on l.id_ruta=rv.id_ruta
         JOIN aeropuerto ao ON p.id_aeropuerto_origen = ao.id
         JOIN aeropuerto ad ON p.id_aeropuerto_destino = ad.id
     """, nativeQuery = true)
