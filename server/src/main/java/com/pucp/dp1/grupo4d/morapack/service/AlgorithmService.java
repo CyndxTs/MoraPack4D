15564bfb38f1f34bf5320845bba5d426940d0be3
Cyndx <150497159+CyndxTs@users.noreply.github.com>
2025-11-12 16:20:15 -0500

    Adción..

--- server/src/main/java/com/pucp/dp1/grupo4d/morapack/service/AlgorithmService.java
+++ server/src/main/java/com/pucp/dp1/grupo4d/morapack/service/AlgorithmService.java
@@ -15,7 +15,6 @@ import com.pucp.dp1.grupo4d.morapack.model.dto.request.ReparameterizationRequest
 import com.pucp.dp1.grupo4d.morapack.model.dto.request.PlanificationRequest;
 import com.pucp.dp1.grupo4d.morapack.model.dto.response.ImportResponse;
 import com.pucp.dp1.grupo4d.morapack.model.dto.response.PlanificationResponse;
-import com.pucp.dp1.grupo4d.morapack.model.dto.response.SimulationResponse;
 import com.pucp.dp1.grupo4d.morapack.model.entity.*;
 import com.pucp.dp1.grupo4d.morapack.service.model.*;
 import jakarta.transaction.Transactional;
@@ -107,7 +106,11 @@ public class AlgorithmService {
             Problematica.MAX_HORAS_RECOJO = parameters.getMaxHorasRecojo();
             Problematica.MAX_HORAS_ESTANCIA = parameters.getMaxHorasEstancia();
             Problematica.MIN_HORAS_ESTANCIA = parameters.getMinHorasEstancia();
+            Problematica.FECHA_HORA_INICIO = parameters.getFechaHoraIni();
+            Problematica.FECHA_HORA_FIN = parameters.getFechaHoraFin();
             Problematica.CODIGOS_DE_ORIGENES = parameters.getCodigosDeOrigenes();
+            GVNS.D_MIN = parameters.getDMin();
+            GVNS.I_MAX = parameters.getIMax();
             GVNS.L_MIN = parameters.getEleMin();
             GVNS.L_MAX = parameters.getEleMax();
             GVNS.K_MIN = parameters.getKMin();
@@ -124,21 +127,22 @@ public class AlgorithmService {
                 aeropuertoAdapter, clienteAdapter, planAdapter, pedidoAdapter
         );
         problematica.cargarDatos();
-        ProblematicResponse pAux = new ProblematicResponse(problematica);
         GVNS gvns = new GVNS();
         gvns.planificar(problematica);
+        if(gvns.getSolucionINI() == null) {
+            return new PlanificationResponse(false, "COLAPSO!");
+        }
         Solucion solucion = gvns.getSolucionVNS();
-        SimulationResponse sAux = new SimulationResponse(solucion);
-        almacenarSolucion(solucion, problematica);
+        guardarSolucion(solucion, problematica);
         problematica.limpiarPools();
         vueloAdapter.clearPools();
         rutaAdapter.clearPools();
         loteAdapter.clearPools();
-        return new PlanificationResponse(true, "Planificación correctamente concluida.", sAux, pAux);
+        return new PlanificationResponse(true, "Planificación correctamente concluida!");
     }
 
     @Transactional
-    public void almacenarSolucion(Solucion solucion, Problematica problematica) {
+    public void guardarSolucion(Solucion solucion, Problematica problematica) {
         if (solucion == null || solucion.getPedidosAtendidos() == null) {
             return;
         }
