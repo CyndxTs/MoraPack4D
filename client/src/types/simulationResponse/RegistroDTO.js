99f7fde677f5a5937a836a460f6b8651dab296fd
JosueSB <a20206724@pucp.edu.pe>
2025-11-25 20:36:01 -0500

    Dtos del front

--- client/src/types/simulationResponse/RegistroDTO.js
+++ client/src/types/simulationResponse/RegistroDTO.js
@@ -1,10 +1,9 @@
-
 /**
  * @typedef {Object} RegistroDTO
  * @property {string} codigo
+ * @property {boolean} sigueVigente
  * @property {string} fechaHoraIngreso
  * @property {string} fechaHoraEgreso
  * @property {string} codLote
  */
-
 export {};
